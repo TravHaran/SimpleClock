625fdd547b27288970eb60bc3301571acdcc4478
Travis Ratnaharan <ratnahat@amazon.com>
2023-08-06 20:11:53 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<unistd.h>  
+using namespace std;
+int main() {
+    int h, m, s, a, err;
+    err=a=0;
+    while(err==0) {
+        cout<<"enter hour : "<<endl;
+        cin>>h;
+        cout<<"enter minutes : "<<endl;
+        cin>>m;
+        cout<<"enter seconds : "<<endl;
+        cin>>s;
+        if(h<24&&m<60&&s<60)
+        err++;
+        else
+        system("clear");
+    }
+    while(a==0)
+    {
+        system("clear");
+        cout<<h<<":"<<m<<":"<<s<<endl;
+        sleep(1);
+        s++;
+        if(s>59) {
+            s=0;
+            m++;
+        }
+        if(m>59) {
+            m=0;
+            h++;
+        }
+        if(h>24) {
+            h=0;
+        }
+    }
+    return 0;
+}
